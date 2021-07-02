2d9cc13e9237a5af36b0b48681ed0fd7a4af2c1e
diana3664 <dianamus2017@gmail.com>
2021-07-03 02:02:56 +0300

    add an onclick listener to the viewJokes list

--- app/src/main/java/com/moringaschool/joke/JokeList.java
+++ app/src/main/java/com/moringaschool/joke/JokeList.java
@@ -3,8 +3,13 @@ package com.moringaschool.joke;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
 
 public class JokeList extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/moringaschool/joke/SignUp.java
+++ app/src/main/java/com/moringaschool/joke/SignUp.java
@@ -2,13 +2,31 @@ package com.moringaschool.joke;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SignUp extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class SignUp extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.viewJokesButton) Button mViewJokesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ButterKnife.bind(this);
+
+        mViewJokesButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        //Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(SignUp.this,JokeList.class);
+        startActivity(intent);
+
     }
 }
