b41be17be574587713b0a3adb6998526b5c1e397
diana3664 <dianamus2017@gmail.com>
2021-07-03 01:21:49 +0300

    add activity called SignUp

--- app/src/main/java/com/moringaschool/joke/MainActivity.java
+++ app/src/main/java/com/moringaschool/joke/MainActivity.java
@@ -4,12 +4,28 @@ package com.moringaschool.joke;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView (R.id.signUpButton) Button msignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        msignUpButton.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+
+    }
+
 }
