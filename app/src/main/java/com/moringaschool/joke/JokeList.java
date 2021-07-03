272fcedf89d8c035d64b31e6cd429e2597c4fc58
diana3664 <dianamus2017@gmail.com>
2021-07-03 03:26:01 +0300

    implementing array adapter

--- app/src/main/java/com/moringaschool/joke/JokeList.java
+++ app/src/main/java/com/moringaschool/joke/JokeList.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -14,6 +16,7 @@ import butterknife.ButterKnife;
 public class JokeList extends AppCompatActivity {
 
     @BindView(R.id.jokeView) TextView mJokeView;
+    @BindView(R.id.listView) ListView mListView;
 
     private String[] jokes = new String[] {"What did the sniper say to his girlfirend after they broke up? I wont miss you ", "Mother's Bistro",
             "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
@@ -27,6 +30,10 @@ public class JokeList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_list);
         ButterKnife.bind(this);
+
+        ArrayAdapter  adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,jokes);
+        mListView.setAdapter(adapter);
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         mJokeView.setText("Hello" +" "+ name+",Jokes to start you off:" );
