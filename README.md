85664545294510af2959ba7a2f27347ae7ecd18f
diana3664 <dianamus2017@gmail.com>
2021-07-05 06:10:22 +0300

    add a README.md file

---
+++ README.md
@@ -0,0 +1,11 @@
+## APP NAME
+Jokes Joke
+##DESCRIPTION
+Application that gives you a category list of jokes from animal jokes to knock knock jokes .Make your day interesting
+##TECHNOLOGIES 
+java
+android-studio
+##SET UP 
+For this project Fork the project,git clone and run in android studio
+#CONTACTS
+dianamus2017@gmail.com
--- app/src/main/java/com/moringaschool/joke/SignUp.java
+++ app/src/main/java/com/moringaschool/joke/SignUp.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,11 +39,24 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
             }else {
                 String name = mSignEditText.getText().toString();
                 Log.d(TAG, name);
+                validateEmail(mSignEditText2);
                 Intent intent = new Intent(SignUp.this, JokeList.class);
                 intent.putExtra("name", name);
                 startActivity(intent);
             }
         }
 
+    }
+
+    private boolean validateEmail(EditText mSignEditText2){
+        String email = mSignEditText2.getText().toString();
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this,"Email validated sucesfully",Toast.LENGTH_SHORT).show();
+            return true;
+
+        }else{
+            Toast.makeText(this,"Invalid Email",Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 }
