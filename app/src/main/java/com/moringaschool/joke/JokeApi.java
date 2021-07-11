4101c720ed083e84a0a30db307365c75fa969d1f
diana3664 <dianamus2017@gmail.com>
2021-07-11 08:03:55 +0300

    add an interface for http call

---
+++ app/src/main/java/com/moringaschool/joke/JokeApi.java
@@ -0,0 +1,15 @@
+package com.moringaschool.joke;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface JokeApi {
+    @GET("categories")
+    Call<CategoriesSearchResponse> getCategories(
+            @Query("categories") String categories
+
+    );
+}
