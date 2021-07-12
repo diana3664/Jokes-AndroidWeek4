ba4e66082222ec2579d5a5605ffa16ed06170247
diana3664 <dianamus2017@gmail.com>
2021-07-12 17:51:34 +0300

    create an if statment in onBindViewHolder adapter to check the joke type

--- app/src/main/java/com/moringaschool/joke/adapters/GeneratedJokesAdapter.java
+++ app/src/main/java/com/moringaschool/joke/adapters/GeneratedJokesAdapter.java
@@ -1,4 +1,55 @@
 package com.moringaschool.joke.adapters;
 
-public class GeneratedJokesAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringaschool.joke.R;
+import com.moringaschool.joke.models.Joke;
+
+import java.util.List;
+
+public class GeneratedJokesAdapter extends RecyclerView.Adapter<GeneratedJokesAdapter.ViewHolder> {
+    List<Joke> allJokes; //from models
+
+    public GeneratedJokesAdapter(List<Joke> jokes){
+        this.allJokes = jokes;
+    }
+    @NonNull
+    @Override
+    public GeneratedJokesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.jokes_view,parent,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GeneratedJokesAdapter.ViewHolder holder, int position) {
+            //check joke types
+        if (allJokes.get(position).getType().equals("single")){
+            holder.firstLine.setText(allJokes.get(position).getJoke());
+            holder.secondLine.setText("");
+        }else{
+            holder.firstLine.setText(allJokes.get(position).getJoke());
+            holder.secondLine.setText(allJokes.get(position).getDelivery());
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return allJokes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView firstLine,secondLine;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            firstLine = itemView.findViewById(R.id.firstLine);
+            secondLine =itemView.findViewById(R.id.secondLine);
+        }
+    }
 }
---
+++ app/src/main/java/com/moringaschool/joke/models/Joke.java
@@ -0,0 +1,41 @@
+package com.moringaschool.joke.models;
+
+//for our recycler view GeneratedJokesAdapter
+public class Joke {
+    private String type;
+    private String setUp;
+    private String delivery;
+    private String joke;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSetUp() {
+        return setUp;
+    }
+
+    public void setSetUp(String setUp) {
+        this.setUp = setUp;
+    }
+
+    public String getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(String delivery) {
+        this.delivery = delivery;
+    }
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+}
