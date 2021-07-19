b3e3f70aaff2dba30bf62a6c4ceae4239850c083
diana3664 <dianamus2017@gmail.com>
2021-07-19 14:00:53 +0300

    Create new user account in Firebase and pass the users email and password

--- app/src/main/java/com/moringaschool/joke/SignUp.java
+++ app/src/main/java/com/moringaschool/joke/SignUp.java
@@ -9,7 +9,10 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,32 +23,64 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.viewJokesButton) Button mViewJokesButton;
     @BindView(R.id.signEditText) EditText mSignEditText;
     @BindView(R.id.signEditText2) EditText mSignEditText2;
+    @BindView(R.id.password) EditText mPassword;
+    @BindView(R.id.cofirmPassword) EditText mConfirmPassword;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
+    private FirebaseAuth mAuth; //creating user in Firebase
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
         ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();//this is to accure tools provided in Firebase Authentification SDK
 
         mViewJokesButton.setOnClickListener(this);
+        mLoginTextView.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
         if(v == mViewJokesButton) {
-            if(mSignEditText.length()==0){
-                mSignEditText.setError("Enter your name");
-            }else if (mSignEditText2.length() == 0){
-                mSignEditText2.setError("Enter email");
-            }else {
-                String name = mSignEditText.getText().toString();
-                Log.d(TAG, name);
-                validateEmail(mSignEditText2);
-                Intent intent = new Intent(SignUp.this, JokeList.class);
-                intent.putExtra("name", name);
-                startActivity(intent);
-            }
+//            if(mSignEditText.length()==0){
+//                mSignEditText.setError("Enter your name");
+//            }else if (mSignEditText2.length() == 0){
+//                mSignEditText2.setError("Enter email");
+//            }else {
+//                String name = mSignEditText.getText().toString();
+//                Log.d(TAG, name);
+//                validateEmail(mSignEditText2);
+//                Intent intent = new Intent(SignUp.this, JokeList.class);
+//                intent.putExtra("name", name);
+//                startActivity(intent);
+//            }
+
+            createNewUser();
+
+        }
+        if(v==mLoginTextView){
+            Intent intent = new Intent(SignUp.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
         }
 
+    }
+
+    private void createNewUser() {
+        final String name = mSignEditText.getText().toString().trim();
+        final String email = mSignEditText2.getText().toString().trim();
+        final String password = mPassword.getText().toString().trim();
+        final String confirmPassword = mConfirmPassword.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this,task -> {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "Authentication successful");
+                    }else {
+                        Toast.makeText(SignUp.this, "Authentification failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private boolean validateEmail(EditText mSignEditText2){
