8349f94198e17655df13fb2bf2a14e323c76266f
diana3664 <dianamus2017@gmail.com>
2021-07-21 15:07:28 +0300

    add Firebase recycler view

--- app/src/main/java/com/moringaschool/Constants.java
+++ app/src/main/java/com/moringaschool/Constants.java
@@ -2,5 +2,6 @@ package com.moringaschool;
 
 public class Constants {
     public static final String FIREBASE_CHILD_JOKES = "jokes";
+    public static final String PREFERENCES_LOCATION_KEY = "location";
 
 }
