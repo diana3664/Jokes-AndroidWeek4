933a200533d764eb322792715865b353ba2c0c22
diana3664 <dianamus2017@gmail.com>
2021-07-24 12:50:51 +0300

    add an activity to display the joke image

---
+++ app/src/main/java/com/moringaschool/JokeImage.java
@@ -0,0 +1,16 @@
+package com.moringaschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.joke.R;
+
+public class JokeImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_image);
+    }
+}
